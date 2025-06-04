b9ab07445a6a670184d3b89e81369dedee132cf4
Arto Bendiken <arto@asimov.so>
2025-06-04 16:16:51 +0400

    Update the project template.

--- llama-index-graph-stores-asimov/llama_index/graph_stores/asimov/__init__.py
+++ llama-index-graph-stores-asimov/llama_index/graph_stores/asimov/__init__.py
@@ -1,6 +1,6 @@
 # This is free and unencumbered software released into the public domain.
 
-"""ASIMOV for LlamaIndex"""
+"""ASIMOV for LlamaIndex: Graph stores."""
 
 from ._version import __version__, __version_tuple__
 from .base import AsimovGraphStore
--- llama-index-indices-managed-asimov/llama_index/indices/managed/asimov/__init__.py
+++ llama-index-indices-managed-asimov/llama_index/indices/managed/asimov/__init__.py
@@ -1,6 +1,6 @@
 # This is free and unencumbered software released into the public domain.
 
-"""ASIMOV for LlamaIndex"""
+"""ASIMOV for LlamaIndex: Managed indexes (indices)."""
 
 from ._version import __version__, __version_tuple__
 from .base import AsimovManagedIndex
