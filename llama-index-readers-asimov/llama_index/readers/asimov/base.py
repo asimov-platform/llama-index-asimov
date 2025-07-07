ed4866449b007473a1468c20efbf2f3d13fb3c4e
AlexSanches1 <sanches2026@gmail.com>
2025-07-07 13:49:16 -0400

    Implement AsimovReader for LlamaIndex

--- llama-index-readers-asimov/llama_index/readers/asimov/base.py
+++ llama-index-readers-asimov/llama_index/readers/asimov/base.py
@@ -1,11 +1,94 @@
 # This is free and unencumbered software released into the public domain.
 
+"""ASIMOV for LlamaIndex: Document loaders."""
+
+from __future__ import annotations
+
+import json
+import logging
+import subprocess
+from typing import List, cast
+
+from .errors import AsimovModuleNotFound
 from llama_index.core.readers.base import BaseReader
 from llama_index.core.schema import Document
-from typing import List
+from pyld import jsonld
+
+logger = logging.getLogger(__name__)
+
+JSONLD_CONTEXT = {
+    "@version": 1.1,
+    "know": "https://know.dev/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+}
+
 
 class AsimovReader(BaseReader):
-    """ASIMOV reader."""
+    """
+    ASIMOV document loader integration.
+
+    Setup:
+        Install ``llama-index-asimov``:
+
+        ```bash
+        pip install -U llama-index-asimov
+        ```
+
+    Instantiate:
+        ```python
+        from llama_index.readers.asimov import AsimovReader
+
+        reader = AsimovReader(
+            module="serpapi",
+            url="https://duckduckgo.com/?q=Isaac+Asimov"
+        )
+        ```
+    """
+
+    def __init__(self, module: str, url: str) -> None:
+        self.module = module
+        self.url = url
 
     def load_data(self) -> List[Document]:
-        return list() # TODO
+        try:
+            result = subprocess.run(
+                [f"asimov-{self.module}-importer", self.url],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            result.check_returncode()
+            output = json.loads(result.stdout)
+            output = cast(dict, jsonld.flatten(output, JSONLD_CONTEXT))
+
+            docs = []
+            for resource in output["@graph"]:
+                resource_id = resource["@id"]
+                page_content = describe(resource)
+                docs.append(Document(text=page_content, id=resource_id, metadata=resource))
+            return docs
+
+        except FileNotFoundError as error:
+            # logger.exception(error)
+            raise AsimovModuleNotFound(self.module) from (error if __debug__ else None)
+        except subprocess.CalledProcessError as error:
+            # logger.exception(error)
+            raise error  # TODO
+        except json.decoder.JSONDecodeError as error:
+            # logger.exception(error)
+            raise error  # TODO
+        except jsonld.JsonLdError as error:
+            # logger.exception(error)
+            raise error  # TODO
+
+
+def describe(resource: dict) -> str:
+    if "know:summary" in resource:
+        return resource["know:summary"]["@value"]
+    if "know:title" in resource:
+        return resource["know:title"]["@value"]
+    if "know:name" in resource:
+        return resource["know:name"]["@value"]
+    if "know:link" in resource:
+        return resource["know:link"]
+    return resource["@id"]
